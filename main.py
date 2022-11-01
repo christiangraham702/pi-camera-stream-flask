e6d6e12ed1e55748a37718d22cb90600b11240bc
christiangraham702 <christian.graham@ufl.edu>
2022-11-01 14:05:39 -0400

    added home page

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
+# Modified by smartbuilds.io
+# Date: 27.09.20
+# Desc: This web application serves a motion JPEG stream
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response, request, send_from_directory
 from camera import VideoCamera
 import os
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
+    # rendering webpage
+    return render_template('home.html')
+
+
+@app.route('/camera_page')
+def live_stream():
+    return render_template('index.html')  # you can customze index.html here
+
 
 def gen(camera):
-    #get camera frame
+    # get camera frame
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 @app.route('/video_feed')
 def video_feed():
@@ -29,10 +38,13 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Take a photo when pressing camera button
+
+
 @app.route('/picture')
 def take_picture():
     pi_camera.take_picture()
     return "None"
+
 
 if __name__ == '__main__':
 
